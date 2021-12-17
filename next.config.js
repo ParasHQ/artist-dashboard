c6a3a0ad1fd019b8f0962c68c8a3b588a96a1caf
EdoWahdana <wahdanaedo@gmail.com>
2021-12-17 15:02:54 +0700

    add: env variable

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	env: {
+		APP_ENV: process.env.APP_ENV,
+		BASE_URL: process.env.BASE_URL,
+		CONTRACT_NAME: process.env.CONTRACT_NAME,
+		API_URL: process.env.API_URL,
+		V2_API_URL: process.env.V2_API_URL,
+		MARKETPLACE_CONTRACT_ID: process.env.MARKETPLACE_CONTRACT_ID,
+		NFT_CONTRACT_ID: process.env.NFT_CONTRACT_ID,
+		WHITELIST_CONTRACT_ID: process.env.WHITELIST_CONTRACT_ID,
+	},
+}
